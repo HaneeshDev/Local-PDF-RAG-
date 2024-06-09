eff75ab26274ce46615f161af9b179248966c53e
HaneeshDev <malepatihaneesh@gmail.com>
2024-06-09 23:52:28 +0530

    added ollama embedding function to use RAG locally

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from langchain_community.document_loaders.pdf import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
+from langchain_communitBedrockEmbeddingsy.embeddings.bedrock import BedrockEmbeddings
+from langchain_community.embeddings.ollama import OllamaEmbeddings
 
 def load_document():
     document_loader = PyPDFDirectoryLoader(".\\Data")
@@ -21,3 +23,15 @@ def split_document(documents: list[Document]):
 chuncks = split_document(documents)
 print(chuncks[0])
 
+
+
+# If you want to use AWS BedRock embedding
+# def get_embedding_function():
+#     embeddings = BedrockEmbeddings(
+#         credentials_profile_name="default", region_name="us-east-1"
+#     )
+#     return embeddings
+
+def get_embedding_function():
+    embeddings = OllamaEmbeddings(model="nomic-embed-text")
+    return embeddings
